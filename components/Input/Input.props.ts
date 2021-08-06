06aeff092b80d30ec96ad21eb07bb27b5e31510c
NoboruWataya123 <jinxw666@gmail.com>
2021-08-06 18:20:40 +0900

    remove components

--- components/Input/Input.props.ts
+++
@@ -1,5 +0,0 @@
-import { DetailedHTMLProps, InputHTMLAttributes, ReactNode } from "react";
-
-export interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-    children?: ReactNode;
-}
--- components/Item/Item.props.ts
+++
@@ -1,5 +0,0 @@
-import { DetailedHTMLProps, HtmlHTMLAttributes, ReactNode } from "react";
-
-export interface ItemProps extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    children?: ReactNode;
-}
